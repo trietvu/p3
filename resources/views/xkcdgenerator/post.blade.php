410215b4c4909dd6f52752acdc5064dc798f982f
Triet Vu <tvu@fas.harvard.edu>
2015-10-23 13:36:08 -0500

    one more tweak

--- resources/views/xkcdgenerator/post.blade.php
+++ resources/views/xkcdgenerator/post.blade.php
@@ -42,7 +42,7 @@
                   @endif
       						<p></p>
       						<p>
-      					 		<label for='numberWords'>Number of words:</lable> <input type='int' name='numberWords'> (Max. of 9. Default is 4 words)<br><br>
+      					 		<label for='numberWords'>Number of words:</lable> <input type='int' name='numberWords' size='1'> (Max. of 9. Default is 4 words)<br><br>
       						</p>
       						<p>
       							<input type='checkbox' name='addNumber' id='addNumber' value='1'>
